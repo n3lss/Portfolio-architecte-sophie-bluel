589cfa2d1bc11bc1db5302b04a50a4b6057cdabb
n3lss <nenes93150@gmail.com>
2023-08-30 16:02:57 +0200

    modification de la modal

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -11,10 +11,25 @@ document.addEventListener('DOMContentLoaded', () => {
 		filterButtons.forEach((button) => button.classList.remove('active'));
 		element.classList.add('active');
 	}
-	
+
 	// recupere les info avec fetch
-	function getfetch(url) {
-		return fetch(url)
+	function getfetch(url, type) {
+		let heade = {};
+		if (!type) {
+			heade = {
+				method: 'GET',
+			};
+		} else if (type) {
+			const headers = {
+				Authorization: `Bearer ${token}`,
+			};
+
+			heade = {
+				method: type,
+				headers: headers,
+			};
+		}
+		return fetch(url, heade)
 			.then((response) => response.json())
 			.catch((error) => console.log('Erreur : ' + error));
 	}
@@ -35,17 +50,17 @@ document.addEventListener('DOMContentLoaded', () => {
 			const objectLength = Object.keys(name).length;
 			for (let categ = 0; categ < objectLength; categ++) {
 				const buttonFilter = document.createElement('input');
-			buttonFilter.value = name[categ].name;
-			buttonFilter.type = 'button';
-			buttonFilter.addEventListener('click', () => {
-				removeColor(buttonFilter);
-				categname = buttonFilter.value;
-				createWorks();
-			});
-			filter.appendChild(buttonFilter);
-				}
+				buttonFilter.value = name[categ].name;
+				buttonFilter.type = 'button';
+				buttonFilter.addEventListener('click', () => {
+					removeColor(buttonFilter);
+					categname = buttonFilter.value;
+					createWorks();
+				});
+				filter.appendChild(buttonFilter);
+			}
 		} catch (error) {
-			console.log("Erreur lors du traitement des filtre:", error);
+			console.log('Erreur lors du traitement des filtre:', error);
 		}
 	}
 
@@ -86,27 +101,122 @@ document.addEventListener('DOMContentLoaded', () => {
 				}
 			}
 		} catch (error) {
-			console.log("Erreur lors du traitement des travaux :", error);
+			console.log('Erreur lors du traitement des travaux :', error);
 		}
 	}
 
-	function buttonModifi() {
+	async function worksDelet() {
+		try {
+			const response = await getfetch('http://localhost:5678/api/works');
+			const objectLength = Object.keys(response).length;
+			const gestionGalery = document.getElementById('gestion-galery');
+			for (let works = 0; works < objectLength; works++) {
+				const figure = document.createElement('figure');
+				const image = document.createElement('img');
+				const figcaption = document.createElement('figcaption');
+				const input = document.createElement('button');
+				const trash = document.createElement('i');
+				trash.className = 'fas fa-trash-alt';
+				trash.ariaHidden = 'true';
+				input.addEventListener('click', () => {
+					getfetch(`http://localhost:5678/api/works/${response[works].id}`, 'DELETE');
+				});
+				input.appendChild(trash);
+				image.src = response[works].imageUrl;
+				image.alt = response[works].title;
+				figcaption.textContent = 'edite';
+				figure.appendChild(image);
+				figure.appendChild(input);
+				figure.appendChild(figcaption);
+				gestionGalery.appendChild(figure);
+			}
+		} catch (error) {
+			console.log('Erreur lors du traitement des travaux :', error);
+		}
+	}
+	function creatGallery() {
+		const containModalDiv = document.getElementById('contain-modal');
+		containModalDiv.innerHTML = '';
+		const headerContainDiv = document.createElement('div');
+		headerContainDiv.className = 'header-contain';
+
+		const h2Element = document.createElement('h2');
+		h2Element.textContent = 'Galerie photo';
+
+		const closeSpan = document.createElement('span');
+		closeSpan.className = 'close';
+		closeSpan.id = 'close';
+		closeSpan.textContent = '×';
+
+		headerContainDiv.appendChild(h2Element);
+		headerContainDiv.appendChild(closeSpan);
+
+		const gestionGaleryDiv = document.createElement('div');
+		gestionGaleryDiv.className = 'gestion-galery';
+		gestionGaleryDiv.id = 'gestion-galery';
+
+		const footerContainDiv = document.createElement('div');
+		footerContainDiv.className = 'footer-contain';
+
+		const addButton = document.createElement('input');
+		addButton.type = 'button';
+		addButton.value = 'Ajouter une photo';
+		addButton.id = 'AddPicture';
+
+		const deleteLink = document.createElement('a');
+		deleteLink.textContent = 'Supprimer la galerie';
+
+		footerContainDiv.appendChild(addButton);
+		footerContainDiv.appendChild(deleteLink);
+
+		containModalDiv.appendChild(headerContainDiv);
+		containModalDiv.appendChild(gestionGaleryDiv);
+		containModalDiv.appendChild(footerContainDiv);
+	}
+	function connected() {
+		//execute la fonction qui crée la modale
+		creatGallery();
+		//
+		//modification de la page quand on et connecter
+		//
+		const login = document.getElementById('login');
 		const modifier = document.createElement('a');
+		const h2Portfolio = portfolio.querySelector('h2');
+		const modal = document.getElementById('modal');
+		login.textContent = 'Logout';
+		login.href = '/FrontEnd/index.html';
 		modifier.textContent = 'Modifier';
 		modifier.href = '#';
-		modifier.addEventListener('click', () => {});
-		const h2Portfolio = portfolio.querySelector('h2');
+		//fonction qui fait aparaitre la modale au click
+		modifier.addEventListener('click', () => {
+			modal.style.display = 'block';
+			//empeche le scroll
+			document.body.style.overflow = 'hidden';
+		});
+		//suprime le token pour se deconnecter
+		login.addEventListener('click', () => {
+			localStorage.removeItem('token');
+		});
 		h2Portfolio.appendChild(document.createTextNode(' '));
 		h2Portfolio.appendChild(modifier);
+		//A l'interieure de la modal
+		const close = document.getElementById('close');
+		const addPicture = document.getElementById('AddPicture');
+		close.addEventListener('click', () => {
+			modal.style.display = 'none';
+			document.body.style.overflow = 'auto';
+		});
+		addPicture.addEventListener('click', () => {
+			//execute la fonction pour afficher l'ajout de nouveaux travaux
+		});
+		//execute la fonction qui affiche tout les travaux et qui premet de les supprimer
+		worksDelet();
 	}
-	console.log(token);
+
 	if (token) {
-		buttonModifi();
-		console.log('connecter!');
+		connected();
 	}
-	createFilter()
+
+	createFilter();
 	createWorks();
 });
-/* window.addEventListener('beforeunload', function () {
-	localStorage.removeItem('token');
-}); */
