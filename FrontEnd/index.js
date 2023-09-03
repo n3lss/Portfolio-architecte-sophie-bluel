c2d5351a48fcff71ee8b6777b688c61ccfb91971
n3lss <nenes93150@gmail.com>
2023-09-03 15:26:01 +0200

    ajout de la modale pour envoyer un projet

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	// recupere les info avec fetch
-	function getfetch(url, type) {
+	function getfetch(url, type, body) {
 		let heade = {};
 		if (!type) {
 			heade = {
@@ -27,6 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			heade = {
 				method: type,
 				headers: headers,
+				body: body,
 			};
 		}
 		return fetch(url, heade)
@@ -104,7 +105,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			console.log('Erreur lors du traitement des travaux :', error);
 		}
 	}
-	// affiche les travaux avec la corbeille pour suprimmer 
+	// affiche les travaux avec la corbeille pour suprimmer
 	async function worksDelete() {
 		try {
 			const response = await getfetch('http://localhost:5678/api/works');
@@ -121,8 +122,8 @@ document.addEventListener('DOMContentLoaded', () => {
 				trash.ariaHidden = 'true';
 				input.addEventListener('click', () => {
 					getfetch(`http://localhost:5678/api/works/${response[works].id}`, 'DELETE');
-					createWorks()
-					worksDelete()
+					createWorks();
+					worksDelete();
 				});
 				input.appendChild(trash);
 				image.src = response[works].imageUrl;
@@ -134,53 +135,159 @@ document.addEventListener('DOMContentLoaded', () => {
 				gestionGalery.appendChild(figure);
 			}
 		} catch (error) {
-			console.log('Erreur lors du traitement des travaux :', error);
+			console.log('Erreur lors du traitement des travaux dans la galery:', error);
 		}
 	}
 	//crée la modal pour la gallery
 	function creatGallery() {
+		//recupere la div de la modal
 		const containModalDiv = document.getElementById('contain-modal');
-		containModalDiv.innerHTML = '';
+		/* containModalDiv.innerHTML = ''; */
+		// crée une div pour le header de la modal
 		const headerContainDiv = document.createElement('div');
 		headerContainDiv.className = 'header-contain';
-
+		//crée un h2 en lui ajoutant le texte galeri
 		const h2Element = document.createElement('h2');
 		h2Element.textContent = 'Galerie photo';
-
-		const closeSpan = document.createElement('span');
-		closeSpan.className = 'close';
-		closeSpan.id = 'close';
-		closeSpan.textContent = '×';
-
-		headerContainDiv.appendChild(h2Element);
-		headerContainDiv.appendChild(closeSpan);
-
+		// crée un i pour le bouton close avec class et id et en text une croix
+		const closei = document.createElement('i');
+		closei.className = 'fa-solid fa-xmark';
+		closei.id = 'close';
+		//crée une div pour afficher les travaux a modifier avec une class et un id
 		const gestionGaleryDiv = document.createElement('div');
 		gestionGaleryDiv.className = 'gestion-galery';
 		gestionGaleryDiv.id = 'gestion-galery';
-
+		// cree une div pour le footer avec une class
 		const footerContainDiv = document.createElement('div');
 		footerContainDiv.className = 'footer-contain';
-
+		// crée un input de type button avec comme value 'ajouter une photo' et un id
 		const addButton = document.createElement('input');
 		addButton.type = 'button';
 		addButton.value = 'Ajouter une photo';
-		addButton.id = 'AddPicture';
-
+		addButton.id = 'AddPictureBtn';
+		// crée une balise a avec comme texte 'Supprimer la galerie'
 		const deleteLink = document.createElement('a');
 		deleteLink.textContent = 'Supprimer la galerie';
-
+		//ajoute les element dans leur div
+		headerContainDiv.appendChild(closei);
+		headerContainDiv.appendChild(h2Element);
 		footerContainDiv.appendChild(addButton);
 		footerContainDiv.appendChild(deleteLink);
-
+		//ajoute toute les div a la modal
 		containModalDiv.appendChild(headerContainDiv);
 		containModalDiv.appendChild(gestionGaleryDiv);
 		containModalDiv.appendChild(footerContainDiv);
+		//execute la fonction qui affiche tout les travaux et qui premet de les supprimer
+		worksDelete();
 	}
-	// execute les fonction quand le token et valide 
+	async function creatAddPicture() {
+		try {
+			const addPicture = document.getElementById('addPicture');
+			const containModal = document.getElementById('contain-modal');
+			//header
+			const headerAddPictureDiv = document.createElement('div');
+			headerAddPictureDiv.className = 'header-add';
+
+			const arrowi = document.createElement('i');
+			arrowi.className = 'fa-solid fa-arrow-left';
+			arrowi.id = 'arrow-left';
+			arrowi.addEventListener('click', () => {
+				addPicture.style.display = 'none';
+				containModal.style.display = 'block';
+			});
+			const closei = document.createElement('i');
+			closei.className = 'fa-solid fa-xmark';
+			closei.id = 'close';
+			closei.addEventListener('click', () => {
+				modal.style.display = 'none';
+				addPicture.style.display = 'none';
+				containModal.style.display = 'block';
+				document.body.style.overflow = 'auto';
+			});
+			const h2Element = document.createElement('h2');
+			h2Element.textContent = 'Ajout photo';
+
+			headerAddPictureDiv.appendChild(arrowi);
+			headerAddPictureDiv.appendChild(closei);
+			headerAddPictureDiv.appendChild(h2Element);
+			addPicture.appendChild(headerAddPictureDiv);
+
+			//formurlaire
+
+			const formAdd = document.createElement('form');
+			formAdd.className = 'formAdd';
+
+			const buttonimg = document.createElement('div');
+			buttonimg.id = 'addImg';
+			buttonimg.className = 'addImg';
+			buttonimg.addEventListener('click', () => {
+				inputAdd.click();
+			});
+			const picturI = document.createElement('i');
+			picturI.className = 'fa-regular fa-image fa-xl';
+
+			const inputAdd = document.createElement('input');
+			inputAdd.type = 'file';
+			inputAdd.id = 'buttonAdd';
+			inputAdd.style.display = 'none';
+			inputAdd.accept = '.jpg, .png';
+
+			const button = document.createElement('button');
+			button.textContent = '+ Ajouter photo';
+			button.type = 'button';
+
+			const type = document.createElement('span');
+			type.textContent = 'jpg, png : 4mo max';
+
+			buttonimg.appendChild(picturI);
+			buttonimg.appendChild(inputAdd);
+			buttonimg.appendChild(button);
+			buttonimg.appendChild(type);
+			formAdd.appendChild(buttonimg);
+
+			const titre = document.createElement('span');
+			titre.textContent = 'Titre';
+			titre.className = 'title';
+
+			const titreName = document.createElement('input');
+			titreName.type = 'text';
+			titreName.className = 'titreName';
+
+			const categorie = document.createElement('span');
+			categorie.textContent = 'Catégorie';
+			categorie.className = 'categorie';
+
+			const select = document.createElement('select');
+			select.id = 'choix';
+			select.name = 'categorie';
+
+			const name = await getfetch('http://localhost:5678/api/categories');
+			const objectLength = Object.keys(name).length;
+			for (let categ = 0; categ < objectLength; categ++) {
+				const option = document.createElement('option');
+				option.value = name[categ].name;
+				option.textContent = name[categ].name;
+				select.appendChild(option);
+			}
+			const valid = document.createElement('input');
+			valid.type = 'submit';
+			valid.placeholder = 'Valider';
+			formAdd.appendChild(titre);
+			formAdd.appendChild(titreName);
+			formAdd.appendChild(categorie);
+			formAdd.appendChild(select);
+			formAdd.appendChild(valid);
+			addPicture.appendChild(formAdd);
+		} catch (error) {
+			console.log('Erreur lors du traitement des travaux dans la galery:', error);
+		}
+	}
+
+	// execute les fonction quand le token et valide
 	function connected() {
 		//execute la fonction qui crée la modale
 		creatGallery();
+		creatAddPicture();
 		//
 		//modification de la page quand on et connecter
 		//
@@ -206,16 +313,18 @@ document.addEventListener('DOMContentLoaded', () => {
 		h2Portfolio.appendChild(modifier);
 		//A l'interieure de la modal
 		const close = document.getElementById('close');
-		const addPicture = document.getElementById('AddPicture');
+		const addPictureBtn = document.getElementById('AddPictureBtn');
+		const containModal = document.getElementById('contain-modal');
 		close.addEventListener('click', () => {
 			modal.style.display = 'none';
 			document.body.style.overflow = 'auto';
 		});
-		addPicture.addEventListener('click', () => {
+		addPictureBtn.addEventListener('click', () => {
+			const addPicture = document.getElementById('addPicture');
+			addPicture.style.display = 'block';
+			containModal.style.display = 'none';
 			//execute la fonction pour afficher l'ajout de nouveaux travaux
 		});
-		//execute la fonction qui affiche tout les travaux et qui premet de les supprimer
-		worksDelete();
 	}
 
 	if (token) {
